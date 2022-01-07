ca88a69a7345b8a04efef002bb77a1a5b415cdb0
vy-shoe <vadimulam6@gatech.edu>
2022-01-06 22:14:04 -0500

    setting up URLs from API in manager model

---
+++ ios-happy-mealz-app/ios-happy-mealz-app/Model/RequestManager.swift
@@ -0,0 +1,18 @@
+//
+//  RequestManager.swift
+//  ios-happy-mealz-app
+//
+//  Created by Vaishu Adi on 1/6/22.
+//
+
+import Foundation
+
+struct RequestManager {
+    let categoryURL     = "www.themealdb.com/api/json/v1/1/categories.php"
+    let mealsURL        = "https://www.themealdb.com/api/json/v1/1/filter.php?c="
+    let idURL           = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="
+    
+    
+    
+    
+}
